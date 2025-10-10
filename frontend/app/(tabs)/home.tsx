4fe4cfd53946903ef952f8e9d6477ce998f3b74a
jimin4801 <wlals4801@gmail.com>
2025-10-10 21:24:37 +0900

    정상화

--- frontend/app/(tabs)/home.tsx
+++ frontend/app/(tabs)/home.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 export default function HomeScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>메인 화면아님</Text>
+      <Text>메인 화면</Text>
     </View>
   );
 }
