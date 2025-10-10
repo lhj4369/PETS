061196b86e2fb030e287ea584cedee96bb4dea29
lhj4369 <dlgkwns66@gmail.com>
2025-10-10 20:12:32 +0900

    first commit

--- frontend/app/(tabs)/challenges.tsx
+++ frontend/app/(tabs)/challenges.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 export default function ChallengesScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>도전 과제 화면</Text>
+      <Text>도전 과제</Text>
     </View>
   );
 }
