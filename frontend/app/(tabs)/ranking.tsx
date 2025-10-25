ea51d3d9c2f61f63675c1478a45b2b873782a4c9
lhj4369 <dlgkwns66@gmail.com>
2025-10-25 20:44:56 +0900

    123

--- frontend/app/(tabs)/ranking.tsx
+++ frontend/app/(tabs)/ranking.tsx
@@ -1,8 +1,10 @@
 import { View, Text } from "react-native";
+import Header from "../../components/Header";
 
 export default function RankingScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Header showBackButton={true} showMenuButton={true} menuType="ranking" />
       <Text>랭킹 화면</Text>
     </View>
   );
--- frontend/app/(tabs)/timer.tsx
+++ frontend/app/(tabs)/timer.tsx
@@ -1,9 +1,11 @@
 import { View, Text } from "react-native";
+import Header from "../../components/Header";
 
 export default function TimerScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>타이머 화면ㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅇ</Text>
+      <Header showBackButton={true} showMenuButton={true} menuType="timer" />
+      <Text>타이머 화면</Text>
     </View>
   );
 }
