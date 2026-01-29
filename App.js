caa84b78ca2a271860a810b55fb3c131ae6aa458
Kachu514 <alsdn6777@naver.com>
2026-01-29 10:52:44 +0900

    소소한거

--- App.js
+++
@@ -1,4 +0,0 @@
-// This file is required for web builds when expo-router is used
-// It must be in the project root (PETS/) for node_modules/expo/AppEntry.js to find it
-import 'expo-router/entry';
-
